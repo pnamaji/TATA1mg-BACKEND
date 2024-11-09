96a1cf85aaa3b938ee47534151db2a7cdbf907f0
Pravin <pnamaji7@gmail.com>
2024-11-09 14:12:36 +0530

    added database

--- BACKEND/settings.py
+++ BACKEND/settings.py
@@ -122,13 +122,18 @@ SITE_ID = 1
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+import dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.parse(config("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
